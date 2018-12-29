f8f256cbccb8e158cda050415ab32b1d95a788b2
Michal Zmudziak <Michal.Zmudziak@ibm.com>
2018-12-29 20:01:01 +0100

    liczby aprzyste

--- main.cpp
+++ main.cpp
@@ -5,10 +5,17 @@ using namespace std;
 
 
 int main() {
-    long n;
+    long n, a;
     cin >> n;
+
     for (int i = 0; i < n; i++) {
-        cout << '$';
+        cin >> a;
+        if (a % 2 == 0) {
+            cout << a;
+        } else {
+            cout << a * 2;
+        }
+        cout << " ";
     }
     return 0;
 }
