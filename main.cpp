2d1665c7fbde8cf71fe744a4bad5737e32b611e2
Michal Zmudziak <Michal.Zmudziak@ibm.com>
2018-12-29 18:32:02 +0100

    szybciej

--- main.cpp
+++ main.cpp
@@ -8,14 +8,13 @@ long long iloscLizakow(long long n) {
      * Jezeli N to potega 2, to zwracamy potęgę ( x^5 => 5 )
      * Jezeli N nie jest potega 2, to znajdujemy kolejną potęgę dwójki i powtarzamy krok 1.
      */
-    cout << "Licze dla " << n << endl;
     if ((ceil(log2(n)) == floor(log2(n)))) {
         // zwracamy potege 2
-        return static_cast<long>(ceil(log2(n)));
+        return static_cast<long long int>(ceil(log2(n)));
     } else {
         // N nie jest potęgą dwójki
         // Znajdź kolejną liczbę która jest potęgą dwójki
-        return iloscLizakow(n + 1);
+        return static_cast<long long int>(ceil(log(n) / log(2)));
     }
 }
 
