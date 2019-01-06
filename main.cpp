d05018223a9572cb99e5f9cad1f4c55cd04e1485
Michal Zmudziak <Michal.Zmudziak@ibm.com>
2019-01-06 19:39:02 +0100

    lol

--- main.cpp
+++ main.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
 #include <math.h>
+#include<map>
 
 using namespace std;
 
 int main() {
     long n;
-    cin >> n;
-    long poprzedniWynik = 0;
-    long wynik;
     long liczba;
+    cin >> n;
+    long tab[6];
+
+    // zerujemy tablicę - w kazdym jej elemencie będzie 0 - bo mamy zero jedynek, dwójek...
+    for (int j = 1; j <= 6; j++) {
+        tab[j] = 0;
+    }
+    // iterujemy N razy wczytując liczbę i wrzucając ją do odpowiedniego miejsca w tablicy, gdzie indeksem jest ocena a wartością jej ilość
     for (int i = 0; i < n; i++) {
         cin >> liczba;
-        if (i == 0) {
-            wynik = liczba;
-        } else {
-            wynik = poprzedniWynik + liczba;
-        }
-        poprzedniWynik = wynik;
-        cout << wynik << " ";
+        tab[liczba] += 1;
     }
+
+    // wypisujemy tablicę
+    for (int k = 1; k <= 6; k++) {
+        cout << tab[k] << " ";
+    }
+
+
+
+
     return 0;
 }
 
