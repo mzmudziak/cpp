2537087ea09d2ba618dc0442bb32792f459d28b4
Michal Zmudziak <Michal.Zmudziak@ibm.com>
2019-01-06 20:17:02 +0100

    pierwzse

--- main.cpp
+++ main.cpp
@@ -4,30 +4,28 @@
 
 using namespace std;
 
+bool czyLiczbaPierwsza(long l) {
+    for (int i = 0; i < sqrt(l); i++) {
+
+        if (i == 0 || i == 1) {
+            continue;
+        }
+        if (l % i != 0) {
+            continue;
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main() {
-    long n;
-    long liczba;
+    long n, l;
     cin >> n;
-    long tab[6];
-
-    // zerujemy tablicę - w kazdym jej elemencie będzie 0 - bo mamy zero jedynek, dwójek...
-    for (int j = 1; j <= 6; j++) {
-        tab[j] = 0;
-    }
-    // iterujemy N razy wczytując liczbę i wrzucając ją do odpowiedniego miejsca w tablicy, gdzie indeksem jest ocena a wartością jej ilość
     for (int i = 0; i < n; i++) {
-        cin >> liczba;
-        tab[liczba] += 1;
+        cin >> l;
+        cout << czyLiczbaPierwsza(l) << " ";
     }
-
-    // wypisujemy tablicę
-    for (int k = 1; k <= 6; k++) {
-        cout << tab[k] << " ";
-    }
-
-
-
-
     return 0;
 }
 
