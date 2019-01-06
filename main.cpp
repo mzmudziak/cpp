02dc3c459bf848310e19e7558e754044861cc217
Michal Zmudziak <Michal.Zmudziak@ibm.com>
2019-01-06 18:28:33 +0100

    max

--- main.cpp
+++ main.cpp
@@ -5,63 +5,20 @@ using namespace std;
 
 
 int main() {
-    long a, b, max = -1000000;
-    char znak;
-    bool czyWypisacNIE = false;
-    cin >> a;
-    cin >> b;
-    long dodane = a + b;
-    long odjete = a - b;
-    long pomnozone = a * b;
-
-    if (dodane == odjete) {
-        czyWypisacNIE = true;
-    }
-    if (dodane == pomnozone) {
-        czyWypisacNIE = true;
-    }
-    if (odjete == pomnozone) {
-        czyWypisacNIE = true;
-    }
-
-    if (czyWypisacNIE) {
-        cout << "NIE";
-        return 0;
-    }
-    if (dodane > max) {
-        max = dodane;
-        znak = '+';
-    }
-    if (odjete > max) {
-        max = odjete;
-        znak = '-';
-    }
-    if (pomnozone > max) {
-        max = pomnozone;
-        znak = '*';
-    }
-    if (a < 0) {
-        cout << "(";
-    }
-    cout << a;
-    if (a < 0) {
-        cout << ")";
-    }
-    cout << znak;
-    if (b < 0) {
-        cout << "(";
-    }
-    cout << b;
-    if (b < 0) {
-        cout << ")";
-    }
-    cout << '=';
-    if (max < 0) {
-        cout << "(";
-    }
-    cout << max;
-    if (max < 0) {
-        cout << ")";
+    long a,b,c;
+    cin >> a >> b >> c;
+    if (a > b) {
+        if (a > c) {
+            cout << a;
+        } else {
+            cout << c;
+        }
+    } else {
+        if (b > c) {
+            cout << b;
+        } else {
+            cout << c;
+        }
     }
     return 0;
 }
