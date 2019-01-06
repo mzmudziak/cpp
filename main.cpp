6584f725f4e2529ced99ab4a78691bb36271f125
Michal Zmudziak <Michal.Zmudziak@ibm.com>
2019-01-06 18:56:39 +0100

    incr order

--- main.cpp
+++ main.cpp
@@ -3,15 +3,32 @@
 
 using namespace std;
 
+long minimum(long a, long b) {
+    return a < b ? a : b;
+}
+
+long minimum(long a, long b, long c) {
+    return minimum(minimum(a, b), c);
+}
+
 long maximum(long a, long b) {
     return a > b ? a : b;
 }
 
+long maximum(long a, long b, long c) {
+    return maximum(maximum(a, b), c);
+}
+
 
 int main() {
-    long a,b,c;
+    long a, b, c;
     cin >> a >> b >> c;
-    cout << maximum(maximum(a, b), c);
+    long min = minimum(a, b, c);
+    long max = maximum(a, b, c);
+    long srodek = a +  b + c - min - max;
+    cout << min << " ";
+    cout << srodek << " ";
+    cout << max;
     return 0;
 }
 
